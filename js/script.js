9e3d0aada5dc557ff334c520a6f4ce68ddd69926
tarekjmt <tarek.bettaieb@esprit.tn>
2018-11-04 12:30:58 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function number(value) {
+    document.form1.result.value += value;
+}
+function cle(value) {
+    document.form1.result.value = value;
+}
+function evalua() {
+    document.form1.result.value = eval(document.form1.result.value);
+}
